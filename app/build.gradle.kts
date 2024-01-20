b40d6c5cf033948d8b21b0f9cea4a53e02080304
Edmilson Da Silva <dsm.edmilson@gmail.com>
2024-01-20 18:24:46 +0100

    He creado el BottomNavigationBar y 3 fragments para el Home, buscar y esttádisticas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "es.edmilson.dream_app"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "es.edmilson.dream_app"
@@ -26,12 +26,20 @@ android {
             )
         }
     }
+
+    buildFeatures{
+        viewBinding = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
     }
 }
 
@@ -41,6 +49,10 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/es/edmilson/dream_app/MainActivity.kt
+++ app/src/main/java/es/edmilson/dream_app/MainActivity.kt
@@ -2,10 +2,42 @@ package es.edmilson.dream_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bottomNavView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        cambiaFragment(HomeFragment())
+        bottomNavView = findViewById(R.id.bottomNavigationView)
+        bottomNavView.setOnItemSelectedListener{menuItem ->
+            when(menuItem.itemId){
+                R.id.home -> {
+                var fragment = HomeFragment()
+                    cambiaFragment(fragment)
+                true
+                }
+                R.id.search -> {
+                var fragment = SearchFragment()
+                    cambiaFragment(fragment)
+                true
+                }
+                R.id.stats -> {
+                var fragment = StatsFragment()
+                    cambiaFragment(fragment)
+                true
+                }
+                else->false
+            }
+        }
+    }
+
+    fun cambiaFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()     // comienza la transaccion
+        transaction.replace(R.id.frame_layout, fragment)  // cambia el fragment actual por el que se le pasa
+        transaction.commit()    // guarda los cambios
     }
 }
