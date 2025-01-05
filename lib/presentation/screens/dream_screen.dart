adc229be834f44ec95c4e16e7f5c6f6deb801050
Edmilson Da Silva <dsm.edmilson@gmail.com>
2025-01-05 12:46:40 +0100

    Fix dreamScreen floatingActionButton

--- lib/presentation/screens/dream_screen.dart
+++ lib/presentation/screens/dream_screen.dart
@@ -78,27 +78,28 @@ class _DreamScreenState extends State<DreamScreen> {
         floatingActionButton: Builder(
           builder: (context) {
             final isKeyboardVisible = MediaQuery.of(context).viewInsets.bottom != 0;
-            return Visibility(
-              visible: !isKeyboardVisible,
-              child: FloatingActionButton(
-                onPressed: () {
-                  if (!formKey.currentState!.validate()) return;
-                  if (context.read<DreamFormBloc>().state.currentIndex == slides.length - 1) {
-                    context.read<DreamFormBloc>().add(const DreamSubmitted());
-                    context.pop();
-                    context.read<DreamHomeBloc>().add(HandleDream(dream: context.read<DreamFormBloc>().state.dream));
-                    return;
-                  }
+            return FloatingActionButton(
+              onPressed: () {
+                if (isKeyboardVisible) {
                   FocusManager.instance.primaryFocus?.unfocus();
-                  swiperController.next(animation: true);
+                  return;
+                }
+                if (!formKey.currentState!.validate()) return;
+                if (context.read<DreamFormBloc>().state.currentIndex == slides.length - 1) {
+                  context.read<DreamFormBloc>().add(const DreamSubmitted());
+                  context.pop();
+                  context.read<DreamHomeBloc>().add(HandleDream(dream: context.read<DreamFormBloc>().state.dream));
+                  return;
+                }
+                FocusManager.instance.primaryFocus?.unfocus();
+                swiperController.next(animation: true);
+              },
+              child: BlocBuilder<DreamFormBloc, DreamFormState>(
+                builder: (context, state) {
+                  return Icon(
+                    state.currentIndex >= (slides.length - 1) ? Icons.arrow_upward : (isKeyboardVisible ? Icons.arrow_downward : Icons.arrow_forward ),
+                  );
                 },
-                child: BlocBuilder<DreamFormBloc, DreamFormState>(
-                  builder: (context, state) {
-                    return Icon(
-                      state.currentIndex >= (slides.length - 1) ? Icons.arrow_upward : Icons.arrow_forward,
-                    );
-                  },
-                ),
               ),
             );
           },
