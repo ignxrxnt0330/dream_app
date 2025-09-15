c660fba08f67b69565cee4cba551f60e476e81c5
Edmilson <dsm.edmilson@gmail.com>
2025-09-15 21:41:56 +0200

    Add scrollable line chart

--- lib/presentation/widgets/stats/charts/custom_line_chart.dart
+++ lib/presentation/widgets/stats/charts/custom_line_chart.dart
@@ -112,68 +112,80 @@ Widget getHorizontalTitles(value, TitleMeta meta) {
         final theme = Theme.of(context);
         final primaryColor = theme.colorScheme.primary;
 
-        return LineChart(
-            LineChartData(
-              lineBarsData: [
-              LineChartBarData(
-                spots: List.generate(entries.length, (i) {
-                  final entry = entries[i];
-                  return FlSpot(i.toDouble(), entry.value.toDouble());
-                  }),
-color: primaryColor,
-barWidth: 2,
-dotData: FlDotData(
-  show: true,
-  getDotPainter: (spot, percent, barData, index) {
-  return FlDotCirclePainter(
-      radius: 4,
-      color: primaryColor,
-      strokeWidth: 1.5,
-      strokeColor: theme.colorScheme.onPrimary,
-      );
-  },
-  ),
-),
-          ],           titlesData: FlTitlesData(
-              leftTitles: AxisTitles(
-                sideTitles: SideTitles(
-                  showTitles: true,
-                  getTitlesWidget: getVerticalTitles,
-                  reservedSize: 36,
-                  ),
-                ),
-              topTitles: AxisTitles(
-                sideTitles: SideTitles(
-                  showTitles: false
-                  ),
-                ),
-              rightTitles: AxisTitles(
-                sideTitles: SideTitles(
-                  showTitles: false,
-                  ),
-                ),
-              bottomTitles: AxisTitles(
-                  sideTitles: SideTitles(
-                    showTitles: true,
-                    getTitlesWidget: getHorizontalTitles,
-                    reservedSize: 32),
-                  ),
+        double width = widget.data.entries.length * 50;
+        double deviceHeight = MediaQuery.of(context).size.height *.8;
+        if(deviceHeight > width) width = deviceHeight;
+
+
+        return SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+          child: SizedBox(
+          height: MediaQuery.of(context).size.width,
+          width: width,
+            child: LineChart(
+                LineChartData(
+                  lineBarsData: [
+                  LineChartBarData(
+                    spots: List.generate(entries.length, (i) {
+                      final entry = entries[i];
+                      return FlSpot(i.toDouble(), entry.value.toDouble());
+                      }),
+            color: primaryColor,
+            barWidth: 2,
+            dotData: FlDotData(
+              show: true,
+              getDotPainter: (spot, percent, barData, index) {
+              return FlDotCirclePainter(
+                  radius: 4,
+                  color: primaryColor,
+                  strokeWidth: 1.5,
+                  strokeColor: theme.colorScheme.onPrimary,
+                  );
+              },
               ),
-              gridData: FlGridData(
-                  show: true,
-                  drawHorizontalLine: true,
-            drawVerticalLine: true,
-            getDrawingHorizontalLine: getHorizontalVerticalLine,
-            getDrawingVerticalLine: getVerticalVerticalLine,
             ),
-        minY: 0,
-        maxY: maxY,
-        baselineY: baselineY,
-        minX: 0,
-        maxX: widget.data.entries.length.toDouble(),
-        baselineX: baselineX,
-        ),
-        duration: Duration.zero,
+              ],           titlesData: FlTitlesData(
+                  leftTitles: AxisTitles(
+                    sideTitles: SideTitles(
+                      showTitles: true,
+                      getTitlesWidget: getVerticalTitles,
+                      reservedSize: 36,
+                      ),
+                    ),
+                  topTitles: AxisTitles(
+                    sideTitles: SideTitles(
+                      showTitles: false
+                      ),
+                    ),
+                  rightTitles: AxisTitles(
+                    sideTitles: SideTitles(
+                      showTitles: false,
+                      ),
+                    ),
+                  bottomTitles: AxisTitles(
+                      sideTitles: SideTitles(
+                        showTitles: true,
+                        getTitlesWidget: getHorizontalTitles,
+                        reservedSize: 32),
+                      ),
+                  ),
+                  gridData: FlGridData(
+                      show: true,
+                      drawHorizontalLine: true,
+                drawVerticalLine: true,
+                getDrawingHorizontalLine: getHorizontalVerticalLine,
+                getDrawingVerticalLine: getVerticalVerticalLine,
+                ),
+            minY: 0,
+            maxY: maxY,
+            baselineY: baselineY,
+            minX: 0,
+            maxX: widget.data.entries.length.toDouble(),
+            baselineX: baselineX,
+            ),
+            duration: Duration.zero,
+            ),
+          ),
         );
     }
 }
