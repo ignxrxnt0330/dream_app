6cc67b34d633032ad471b737f43ba4ebd4fe5513
Edmilson <dsm.edmilson@gmail.com>
2025-10-01 13:09:59 +0200

    Fix jump focus title desc

--- lib/presentation/views/dream/dream_form_view.dart
+++ lib/presentation/views/dream/dream_form_view.dart
@@ -4,6 +4,7 @@ import 'package:dream_app/domain/entities/dream/dream.dart';
 import 'package:dream_app/infrastructure/datasources/isar_datasource.dart';
 import 'package:dream_app/presentation/blocs/blocs.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 import 'package:multi_trigger_autocomplete_plus/multi_trigger_autocomplete_plus.dart';
@@ -13,7 +14,7 @@ class DreamFormView extends StatefulWidget {
   const DreamFormView({super.key});
 
   @override
-  State<DreamFormView> createState() => _DreamFormViewState();
+    State<DreamFormView> createState() => _DreamFormViewState();
 }
 
 class _DreamFormViewState extends State<DreamFormView> {
@@ -24,25 +25,28 @@ class _DreamFormViewState extends State<DreamFormView> {
   List<String> names = [];
   List<String> allNames = [];
 
-  @override
-  void initState() {
-    super.initState();
-    Dream dream = context.read<DreamFormBloc>().state.dream;
-    titleController.text = dream.title ;
-    descriptionController.text = dream.description;
-    names = dream.names;
+  final _descriptionKey = GlobalKey<_DescriptionRowState>();
 
-    IsarDatasource().getAllNames().then((names){
-        allNames = names;
-        allNames.sort((a,b) => a.compareTo(b));
-        setState(() { });
-        });
-  }
+
+  @override
+    void initState() {
+      super.initState();
+      Dream dream = context.read<DreamFormBloc>().state.dream;
+      titleController.text = dream.title ;
+      descriptionController.text = dream.description;
+      names = dream.names;
+
+      IsarDatasource().getAllNames().then((names){
+          allNames = names;
+          allNames.sort((a,b) => a.compareTo(b));
+          setState(() { });
+          });
+    }
 
   void save() {
     Dream dream = context.read<DreamFormBloc>().state.dream.copyWith(
-          title: titleController.text != "" ? titleController.text : context.read<AppConfigBloc>().state.defaultTitle,
-          description: descriptionController.text,
+        title: titleController.text != "" ? titleController.text : context.read<AppConfigBloc>().state.defaultTitle,
+        description: descriptionController.text,
         );
     context.read<DreamFormBloc>().add(FieldChanged(dream));
   }
@@ -53,66 +57,68 @@ class _DreamFormViewState extends State<DreamFormView> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
-        child: Column(
-          children: [
-            const SizedBox(height: 10),
-            const _DateTimeRow(),
-            const SizedBox(height: 20),
-            _TitleRow(titleController, descriptionFocusNode, context.read<DreamFormBloc>().state.dream.id == -9223372036854775808), // -9223372036854775808 is null
-            const SizedBox(height: 20),
-            _DescriptionRow(descriptionController, save, descriptionFocusNode, allNames),
-            const SizedBox(height: 20),
-            _NamesRow(names),
-          ],
-        ),
-      ),
-    );
-  }
+    Widget build(BuildContext context) {
+      return SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Column(
+              children: [
+              const SizedBox(height: 10),
+              const _DateTimeRow(),
+              const SizedBox(height: 20),
+              _TitleRow(titleController, context.read<DreamFormBloc>().state.dream.id == -9223372036854775808,  () {
+                _descriptionKey.currentState?.descriptionFocusNode?.requestFocus();
+                },),
+              const SizedBox(height: 20),
+              _DescriptionRow(key: _descriptionKey,descriptionController, save, descriptionFocusNode, allNames),
+              const SizedBox(height: 20),
+              _NamesRow(names),
+              ],
+              ),
+            ),
+          );
+    }
 }
 
 class _TitleRow extends StatelessWidget {
   final TextEditingController controller;
-  final FocusNode nextFocusNode;
   final bool autofocus;
-  const _TitleRow(this.controller, this.nextFocusNode, this.autofocus);
+  final VoidCallback onNextFocus;
+  const _TitleRow(this.controller, this.autofocus, this.onNextFocus);
 
   @override
-  Widget build(BuildContext context) {
-    return Material(
-      child: TextFormField(
-        autofocus: controller.text.isEmpty ? autofocus : false,
-        controller: controller,
-        onTapOutside:(event){
-        context.read<DreamFormBloc>().add(ValidChanged(valid: controller.text.isNotEmpty));
-        FocusScope.of(context).unfocus();
-        },
-        decoration: InputDecoration(
-          labelText: 'Title',
-          hintText: context.read<AppConfigBloc>().state.defaultTitle.isNotEmpty ? context.read<AppConfigBloc>().state.defaultTitle : null,
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(20),
-          ),
-        ),
-        maxLength: 40,
-        maxLines: 1,
-        validator: (value) {
-           if ((value == null || value.isEmpty) && context.read<AppConfigBloc>().state.defaultTitle.isEmpty) {
-             context.read<DreamFormBloc>().add(ValidChanged(valid: false));
-             return "empty";
-           }
-           context.read<DreamFormBloc>().add(ValidChanged(valid: true));
-          return null;
-        },
-        onFieldSubmitted: (_) {
-          FocusScope.of(context).requestFocus(nextFocusNode);
-        },
-      ),
-    );
-  }
+    Widget build(BuildContext context) {
+      return Material(
+          child: TextFormField(
+            autofocus: controller.text.isEmpty ? autofocus : false,
+            controller: controller,
+            onTapOutside:(event){
+            context.read<DreamFormBloc>().add(ValidChanged(valid: controller.text.isNotEmpty));
+            FocusScope.of(context).unfocus();
+            },
+decoration: InputDecoration(
+              labelText: 'Title',
+              hintText: context.read<AppConfigBloc>().state.defaultTitle.isNotEmpty ? context.read<AppConfigBloc>().state.defaultTitle : null,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+maxLength: 40,
+maxLines: 1,
+validator: (value) {
+if ((value == null || value.isEmpty) && context.read<AppConfigBloc>().state.defaultTitle.isEmpty) {
+context.read<DreamFormBloc>().add(ValidChanged(valid: false));
+return "empty";
+}
+context.read<DreamFormBloc>().add(ValidChanged(valid: true));
+return null;
+},
+onFieldSubmitted: (_) {
+                    onNextFocus();
+                  },
+                  ),
+                  );
+}
 }
 
 class _DescriptionRow extends StatefulWidget {
@@ -120,32 +126,33 @@ class _DescriptionRow extends StatefulWidget {
   final Function save;
   final FocusNode descriptionFocusNode;
   final List<String> allNames;
-  const _DescriptionRow(this.controller, this.save, this.descriptionFocusNode, this.allNames);
+  const _DescriptionRow(this.controller, this.save, this.descriptionFocusNode, this.allNames, {super.key});
 
   @override
-  State<_DescriptionRow> createState() => _DescriptionRowState();
+    State<_DescriptionRow> createState() => _DescriptionRowState();
 }
 
 class _DescriptionRowState extends State<_DescriptionRow> {
+  FocusNode? descriptionFocusNode;
 
   @override
-  Widget build(BuildContext context) {
-    return MultiTriggerAutocomplete(
-        optionsAlignment: OptionsAlignment.topStart,
-        autocompleteTriggers: [
-        //TODO: AutocompleteTrigger space, hide open option ¿?
-        AutocompleteTrigger(
-          trigger: '@',
-          triggerEnd: " ",
-          optionsViewBuilder: (context, autocompleteQuery, controller) {
-          List<String> names = widget.allNames.where((n) => n.toLowerCase().contains(autocompleteQuery.query.toLowerCase())).toList();
-          return Opacity(
-          opacity: 0.75,
-            child: Container(
-                color: Colors.black,
-                constraints: BoxConstraints(
-                  maxHeight: MediaQuery.of(context).size.height * 0.15,
-                  ),
+    Widget build(BuildContext context) {
+      return MultiTriggerAutocomplete(
+          optionsAlignment: OptionsAlignment.topStart,
+          autocompleteTriggers: [
+          //TODO: AutocompleteTrigger space, hide open option ¿?
+          AutocompleteTrigger(
+            trigger: '@',
+            triggerEnd: " ",
+            optionsViewBuilder: (context, autocompleteQuery, controller) {
+            List<String> names = widget.allNames.where((n) => n.toLowerCase().contains(autocompleteQuery.query.toLowerCase())).toList();
+            return Opacity(
+                opacity: 0.75,
+                child: Container(
+                  color: Colors.black,
+                  constraints: BoxConstraints(
+                    maxHeight: MediaQuery.of(context).size.height * 0.15,
+                    ),
                   child: ListView.builder(
                     shrinkWrap: true,
                     itemBuilder: (context, index) {
@@ -157,44 +164,45 @@ class _DescriptionRowState extends State<_DescriptionRow> {
                         return autocomplete.acceptAutocompleteOption(name);
                         });
                     },
-                itemCount: names.length,
-              ),
+itemCount: names.length,
+),
+                  ),
+                );
+            },
             ),
-          );
-          },
-          ),
-        ],
-        // Add the text field widget you want to use for autocomplete
-        fieldViewBuilder: (context, controller, focusNode) {
-        return TextFormField(
-                controller: controller,
-                focusNode: focusNode,
-                onTapOutside:(event){
-                context.read<DreamFormBloc>().add(ValidChanged(valid: controller.text.isNotEmpty));
-                FocusScope.of(context).unfocus();
-                },
-decoration: InputDecoration(
-              labelText: 'Description',
-              hintText: "asdasdasd...",
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(20),
-                ),
-              ),
-minLines: 5,
-maxLines: 20,
-validator: (value) {
-if (value == null || value.isEmpty) {
-return "empty";
-}
-widget.controller.text = controller.text;
-widget.save();
-              return null;
-              }
-  );
-},
-  );
+            ],
+fieldViewBuilder: (context, controller, focusNode) {
+  descriptionFocusNode = focusNode;
 
-  }
+  return TextFormField(
+      controller: controller,
+      focusNode: focusNode,
+      onTapOutside: (event) {
+        context.read<DreamFormBloc>().add(
+            ValidChanged(valid: controller.text.isNotEmpty));
+        FocusScope.of(context).unfocus();
+      },
+      decoration: InputDecoration(
+        labelText: 'Description',
+        hintText: 'Write your dream...',
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+      ),
+      minLines: 5,
+      maxLines: 20,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'empty';
+        }
+        widget.controller.text = controller.text;
+        widget.save();
+        return null;
+      },
+  );
+});
+
+}
 }
 
 class _NamesRow extends StatelessWidget {
@@ -202,56 +210,56 @@ class _NamesRow extends StatelessWidget {
   const _NamesRow(this.names);
 
   @override
-  Widget build(BuildContext context) {
-    return Wrap(
-      spacing: 5,
-      crossAxisAlignment: WrapCrossAlignment.start,
-      children: names.map((name) => Chip(label: Text(name))).toList(),
-    );
-  }
+    Widget build(BuildContext context) {
+      return Wrap(
+          spacing: 5,
+          crossAxisAlignment: WrapCrossAlignment.start,
+          children: names.map((name) => Chip(label: Text(name))).toList(),
+          );
+    }
 }
 
 class _DateTimeRow extends StatefulWidget {
   const _DateTimeRow();
 
   @override
-  State<_DateTimeRow> createState() => _DateTimeRowState();
+    State<_DateTimeRow> createState() => _DateTimeRowState();
 }
 
 class _DateTimeRowState extends State<_DateTimeRow> {
   late DateTime value;
   @override
-  void initState() {
-    super.initState();
-    value = context.read<DreamFormBloc>().state.dream.date ?? DateTime.now();
-  }
+    void initState() {
+      super.initState();
+      value = context.read<DreamFormBloc>().state.dream.date ?? DateTime.now();
+    }
 
   @override
-  Widget build(BuildContext context) {
-    return Material(
-      child: DateTimeFormField(
-        dateFormat: DateFormat('dd-MM-yyyy HH:mm'),
-        decoration: InputDecoration(
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(20),
+    Widget build(BuildContext context) {
+      return Material(
+          child: DateTimeFormField(
+            dateFormat: DateFormat('dd-MM-yyyy HH:mm'),
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+                ),
+              labelText: 'Date',
+              ),
+            initialPickerDateTime: value,
+            initialValue: value,
+            onChanged: (DateTime? value) {
+            if (value == null) return;
+            Dream dream = context.read<DreamFormBloc>().state.dream;
+            dream = dream.copyWith(date: value);
+            context.read<DreamFormBloc>().add(FieldChanged(dream));
+            },
+validator: (value) {
+if (value == null) {
+return "empty";
+}
+return null;
+          },
           ),
-          labelText: 'Date',
-        ),
-        initialPickerDateTime: value,
-        initialValue: value,
-        onChanged: (DateTime? value) {
-          if (value == null) return;
-          Dream dream = context.read<DreamFormBloc>().state.dream;
-          dream = dream.copyWith(date: value);
-          context.read<DreamFormBloc>().add(FieldChanged(dream));
-        },
-        validator: (value) {
-          if (value == null) {
-            return "empty";
-          }
-          return null;
-        },
-      ),
-    );
-  }
+          );
+}
 }
